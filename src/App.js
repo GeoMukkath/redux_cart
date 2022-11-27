15829b50de6f7f402494684af660758e07769e19
geomukkath <geomukkath@yandex.com>
2022-11-27 21:43:26 +0530

    Added modal functionality for cart

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import CartContainer from "./components/CartContainer";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { totalCount } from "./features/cart/cartSlice";
+import Modal from "./components/Modal";
 
 function App() {
   const { cartItems } = useSelector((state) => state.cart);
+  const { isOpen } = useSelector((state) => state.modal);
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(totalCount());
@@ -14,6 +17,7 @@ function App() {
   }, [cartItems]);
   return (
     <>
+      {isOpen ? <Modal /> : 0}
       <Navbar />
       <CartContainer />
     </>
